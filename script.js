88c942043823fe0f15236fe3b142e26a202eda52
Alexandre <alexandrefaria3012@gmail.com>
2022-08-22 09:48:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const handleFocus = ({target}) => {
     const span = target.previousElementSibling;
     span.classList.add('span-active');
 }
+
+const handleChange = () => {
+    const [username,password] = inputs;
+
+    if(username.value && password.value.length >= 8)
+    {
+        button.removeAtribute('disabled');
+    }
+}
+
+inputs.forEach((input) => input.addEventListener('input',handleChange));
